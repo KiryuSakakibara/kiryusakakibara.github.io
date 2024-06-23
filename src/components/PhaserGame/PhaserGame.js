105b46d1eb9c142ab49944d4c329765dafcc7144
Kiryu Sakakibara <kiryusaka@gmail.com>
2024-06-23 16:25:08 -0400

    Added time step delta to phaser game page

--- src/components/PhaserGame/PhaserGame.js
+++ src/components/PhaserGame/PhaserGame.js
@@ -1,9 +1,17 @@
 import React from "react";
 import Particle from "../Particle";
+import Iframe from "react-iframe";
 
 function PhaserGame() {
     return (
-        <div>
+        <div id="PhaserGame">
+            <Iframe
+                title="Time Step Delta"
+                src="https://html-classic.itch.zone/html/10319512/dist/index.html"
+                width="100%"
+                height="100%"
+                allowFullScreen
+            ></Iframe>
             <Particle/>
         </div>
     )
